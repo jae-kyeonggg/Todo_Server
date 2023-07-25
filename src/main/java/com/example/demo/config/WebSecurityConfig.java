8babeaceab2503950e81f05aef019f2d4504085e
jae-kyeonggg <hanjaekyung56@gmail.com>
2023-07-25 14:47:13 +0900

    add Github Login to frontend

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -3,12 +3,15 @@ package com.example.demo.config;
 import com.example.demo.security.JwtAuthenticationFilter;
 import com.example.demo.security.OAuthSuccessHandler;
 import com.example.demo.security.OAuthUserServiceImpl;
+import com.example.demo.security.RedirectUrlCookieFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
@@ -23,6 +26,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private OAuthSuccessHandler oAuthSuccessHandler;
+
+    @Autowired
+    private RedirectUrlCookieFilter redirectUrlFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -44,11 +50,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .redirectionEndpoint()
                 .baseUri("/oauth2/callback/*")
                 .and()
+                .authorizationEndpoint()
+                .baseUri("/auth/authorize")
+                .and()
                 .userInfoEndpoint()
                 .userService(oAuthUserService)
                 .and()
-                .successHandler(oAuthSuccessHandler);
+                .successHandler(oAuthSuccessHandler)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new Http403ForbiddenEntryPoint());
 
         http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+        http.addFilterBefore(redirectUrlFilter, OAuth2AuthorizationRequestRedirectFilter.class);
     }
 }
